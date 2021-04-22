9bebe4482200b7e30525ede4de0893a237122a0f
UselessFish99 <gianlucapalmarozza@gmail.com>
2021-04-22 14:07:13 +0200

    commit message

--- try_it_yourself_queries.sql
+++ try_it_yourself_queries.sql
@@ -1,3 +1,4 @@
+--chap 1
 CREATE TABLE animals (
 	id bigserial,
 	animal varchar(50)
@@ -39,12 +40,30 @@ INSERT INTO animal_specifics (animal, animal_name, weight, arrival_date) VALUES
 	
 SELECT * FROM animal_specifics
 
+-- chap 2 
 
+SELECT school, first_name, last_name
+FROM teachers
+ORDER BY school, last_name;
 
+SELECT first_name, last_name, school, salary
+FROM teachers
+WHERE first_name LIKE 'S%' 
+      AND salary > 40000;
+
+SELECT last_name, first_name, school, hire_date, salary
+FROM teachers
+WHERE hire_date >= '2010-01-01'
+ORDER BY salary DESC;
+
+--chapter 3
+--1. real
+--2.varchar(50)
+--3. you will get an error
 CREATE TABLE char_data_types (
  varchar_column varchar(10),
  char_column char(10),
- text_column text
+ text_column varchar(50)
 );
 
 INSERT INTO char_data_types
@@ -100,14 +119,150 @@ interval_column,
 timestamp_column - interval_column AS new_date
 FROM date_time_types;
 
---chapter 3 try it yourself
---1. real
---2.varchar(50)
---3. you will get an error
+-- chap 4
 
 COPY imaginary_table 
 TO 'C:\Users\Y520\Documents\postgreSQL\imaginary.txt'
 WITH (FORMAT CSV, HEADER, DELIMITER '#');
+);
+
+-- chap 5 
+SELECT 3.14 * 5 ^ 2;
+
+SELECT 3.14 * (5 ^ 2);
+
+SELECT geo_name,
+ state_us_abbreviation AS "st",
+ (CAST (p0010006 AS numeric(8,1)) / p0010001) * 100 AS "pct_asian"
+FROM us_counties_2010
+ORDER BY "pct_asian" DESC;
+
+
+SELECT geo_name,
+       state_us_abbreviation,
+       p0010001 AS total_population,
+       p0010005 AS american_indian_alaska_native_alone,
+       (CAST (p0010005 AS numeric(8,1)) / p0010001) * 100
+           AS percent_american_indian_alaska_native_alone
+FROM us_counties_2010
+WHERE state_us_abbreviation = 'NY'
+ORDER BY percent_american_indian_alaska_native_alone DESC;
+
+SELECT percentile_cont(.5)
+        WITHIN GROUP (ORDER BY p0010001)
+FROM us_counties_2010
+WHERE state_us_abbreviation = 'NY';
+
+SELECT percentile_cont(.5)
+        WITHIN GROUP (ORDER BY p0010001)
+FROM us_counties_2010
+WHERE state_us_abbreviation = 'CA';
+
+SELECT state_us_abbreviation,
+       percentile_cont(0.5)
+          WITHIN GROUP (ORDER BY p0010001) AS median
+FROM us_counties_2010
+WHERE state_us_abbreviation IN ('NY', 'CA')
+GROUP BY state_us_abbreviation;
+
+--6
+
+SELECT c2010.geo_name,
+       c2010.state_us_abbreviation,
+       c2000.geo_name
+FROM us_counties_2010 c2010 LEFT JOIN us_counties_2000 c2000
+ON c2010.state_fips = c2000.state_fips
+   AND c2010.county_fips = c2000.county_fips
+WHERE c2000.geo_name IS NULL;
+
+SELECT c2010.geo_name,
+       c2000.geo_name,
+       c2000.state_us_abbreviation
+FROM us_counties_2010 c2010 RIGHT JOIN us_counties_2000 c2000
+ON c2010.state_fips = c2000.state_fips
+   AND c2010.county_fips = c2000.county_fips
+WHERE c2010.geo_name IS NULL;
+
+
+SELECT median(round( (CAST(c2010.p0010001 AS numeric(8,1)) - c2000.p0010001)
+           / c2000.p0010001 * 100, 1 )) AS median_pct_change
+FROM us_counties_2010 c2010 INNER JOIN us_counties_2000 c2000
+ON c2010.state_fips = c2000.state_fips
+   AND c2010.county_fips = c2000.county_fips;
+
+
+SELECT percentile_cont(.5)
+       WITHIN GROUP (ORDER BY round( (CAST(c2010.p0010001 AS numeric(8,1)) - c2000.p0010001)
+           / c2000.p0010001 * 100, 1 )) AS percentile_50th
+FROM us_counties_2010 c2010 INNER JOIN us_counties_2000 c2000
+ON c2010.state_fips = c2000.state_fips
+   AND c2010.county_fips = c2000.county_fips;
+
+SELECT c2010.geo_name,
+       c2010.state_us_abbreviation,
+       c2010.p0010001 AS pop_2010,
+       c2000.p0010001 AS pop_2000,
+       c2010.p0010001 - c2000.p0010001 AS raw_change,
+       round( (CAST(c2010.p0010001 AS DECIMAL(8,1)) - c2000.p0010001)
+           / c2000.p0010001 * 100, 1 ) AS pct_change
+FROM us_counties_2010 c2010 INNER JOIN us_counties_2000 c2000
+ON c2010.state_fips = c2000.state_fips
+   AND c2010.county_fips = c2000.county_fips
+ORDER BY pct_change ASC;
+
+--7
+CREATE TABLE albums (
+    album_id bigserial,
+    album_catalog_code varchar(100),
+    album_title text,
+    album_artist text,
+    album_time interval,
+    album_release_date date,
+    album_genre varchar(40),
+    album_description text
+);
+
+CREATE TABLE songs (
+    song_id bigserial,
+    song_title text,
+    song_artist text,
+    album_id bigint
+);
+
+CREATE TABLE albums (
+    album_id bigserial,
+    album_catalog_code varchar(100) NOT NULL,
+    album_title text NOT NULL,
+    album_artist text NOT NULL,
+    album_release_date date,
+    album_genre varchar(40),
+    album_description text,
+    CONSTRAINT album_id_key PRIMARY KEY (album_id),
+    CONSTRAINT release_date_check CHECK (album_release_date > '1/1/1925')
+);
+
+CREATE TABLE songs (
+    song_id bigserial,
+    song_title text NOT NULL,
+    song_artist text NOT NULL,
+    album_id bigint REFERENCES albums (album_id),
+    CONSTRAINT song_id_key PRIMARY KEY (song_id)
+);
+----------------------------------------------------------------------------
+SELECT state_us_abbreviation,
+       percentile_cont(0.5)
+          WITHIN GROUP (ORDER BY p0010001) AS median
+FROM us_counties_2010
+GROUP BY state_us_abbreviation;
+
+COPY (
+    SELECT geo_name, state_us_abbreviation, housing_unit_count_100_percent
+    FROM us_counties_2010 ORDER BY housing_unit_count_100_percent DESC LIMIT 20
+     )
+TO 'C:\Users\Y520\Documents\postgreSQL\us_counties_housing_export.txt'
+WITH (FORMAT CSV, HEADER);
+
+
 
 COPY (SELECT geo_name, state_us_abbreviation, housing_unit_count_100_percent
     FROM us_counties_2010 ORDER BY housing_unit_count_100_percent DESC LIMIT 20)
