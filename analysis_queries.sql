cb90494ef26c9272432647be4fe71813a7507313
UselessFish99 <gianlucapalmarozza@gmail.com>
2021-04-22 13:15:27 +0200

    fourth commit

--- analysis_queries.sql
+++ analysis_queries.sql
@@ -470,3 +470,381 @@ EXPLAIN ANALYZE SELECT * FROM new_york_addresses
 WHERE street = '52 STREET';
 EXPLAIN ANALYZE SELECT * FROM new_york_addresses
 WHERE street = 'ZWICKY AVENUE';
+
+
+CREATE TABLE pls_fy2014_pupld14a (
+    stabr varchar(2) NOT NULL,
+    fscskey varchar(6) CONSTRAINT fscskey2014_key PRIMARY KEY,
+    libid varchar(20) NOT NULL,
+    libname varchar(100) NOT NULL,
+    obereg varchar(2) NOT NULL,
+    rstatus integer NOT NULL,
+    statstru varchar(2) NOT NULL,
+    statname varchar(2) NOT NULL,
+    stataddr varchar(2) NOT NULL,
+    longitud numeric(10,7) NOT NULL,
+    latitude numeric(10,7) NOT NULL,
+    fipsst varchar(2) NOT NULL,
+    fipsco varchar(3) NOT NULL,
+    address varchar(35) NOT NULL,
+    city varchar(20) NOT NULL,
+    zip varchar(5) NOT NULL,
+    zip4 varchar(4) NOT NULL,
+    cnty varchar(20) NOT NULL,
+    phone varchar(10) NOT NULL,
+    c_relatn varchar(2) NOT NULL,
+    c_legbas varchar(2) NOT NULL,
+    c_admin varchar(2) NOT NULL,
+    geocode varchar(3) NOT NULL,
+    lsabound varchar(1) NOT NULL,
+    startdat varchar(10),
+    enddate varchar(10),
+    popu_lsa integer NOT NULL,
+    centlib integer NOT NULL,
+    branlib integer NOT NULL,
+    bkmob integer NOT NULL,
+    master numeric(8,2) NOT NULL,
+    libraria numeric(8,2) NOT NULL,
+    totstaff numeric(8,2) NOT NULL,
+    locgvt integer NOT NULL,
+    stgvt integer NOT NULL,
+    fedgvt integer NOT NULL,
+    totincm integer NOT NULL,
+    salaries integer,
+    benefit integer,
+    staffexp integer,
+    prmatexp integer NOT NULL,
+    elmatexp integer NOT NULL,
+    totexpco integer NOT NULL,
+    totopexp integer NOT NULL,
+    lcap_rev integer NOT NULL,
+    scap_rev integer NOT NULL,
+    fcap_rev integer NOT NULL,
+    cap_rev integer NOT NULL,
+    capital integer NOT NULL,
+    bkvol integer NOT NULL,
+    ebook integer NOT NULL,
+    audio_ph integer NOT NULL,
+    audio_dl float NOT NULL,
+    video_ph integer NOT NULL,
+    video_dl float NOT NULL,
+    databases integer NOT NULL,
+    subscrip integer NOT NULL,
+    hrs_open integer NOT NULL,
+    visits integer NOT NULL,
+    referenc integer NOT NULL,
+    regbor integer NOT NULL,
+    totcir integer NOT NULL,
+    kidcircl integer NOT NULL,
+    elmatcir integer NOT NULL,
+    loanto integer NOT NULL,
+    loanfm integer NOT NULL,
+    totpro integer NOT NULL,
+    totatten integer NOT NULL,
+    gpterms integer NOT NULL,
+    pitusr integer NOT NULL,
+    wifisess integer NOT NULL,
+    yr_sub integer NOT NULL
+);
+
+CREATE INDEX libname2014_idx ON pls_fy2014_pupld14a (libname);
+CREATE INDEX stabr2014_idx ON pls_fy2014_pupld14a (stabr);
+CREATE INDEX city2014_idx ON pls_fy2014_pupld14a (city);
+CREATE INDEX visits2014_idx ON pls_fy2014_pupld14a (visits);
+
+COPY pls_fy2014_pupld14a
+FROM 'C:\Users\Y520\Documents\postgreSQL\pls_fy2014_pupld14a.csv'
+WITH (FORMAT CSV, HEADER);
+
+
+CREATE TABLE pls_fy2009_pupld09a (
+    stabr varchar(2) NOT NULL,
+    fscskey varchar(6) CONSTRAINT fscskey2009_key PRIMARY KEY,
+    libid varchar(20) NOT NULL,
+    libname varchar(100) NOT NULL,
+    address varchar(35) NOT NULL,
+    city varchar(20) NOT NULL,
+    zip varchar(5) NOT NULL,
+    zip4 varchar(4) NOT NULL,
+    cnty varchar(20) NOT NULL,
+    phone varchar(10) NOT NULL,
+    c_relatn varchar(2) NOT NULL,
+    c_legbas varchar(2) NOT NULL,
+    c_admin varchar(2) NOT NULL,
+    geocode varchar(3) NOT NULL,
+    lsabound varchar(1) NOT NULL,
+    startdat varchar(10),
+    enddate varchar(10),
+    popu_lsa integer NOT NULL,
+    centlib integer NOT NULL,
+    branlib integer NOT NULL,
+    bkmob integer NOT NULL,
+    master numeric(8,2) NOT NULL,
+    libraria numeric(8,2) NOT NULL,
+    totstaff numeric(8,2) NOT NULL,
+    locgvt integer NOT NULL,
+    stgvt integer NOT NULL,
+    fedgvt integer NOT NULL,
+    totincm integer NOT NULL,
+    salaries integer,
+    benefit integer,
+    staffexp integer,
+    prmatexp integer NOT NULL,
+    elmatexp integer NOT NULL,
+    totexpco integer NOT NULL,
+    totopexp integer NOT NULL,
+    lcap_rev integer NOT NULL,
+    scap_rev integer NOT NULL,
+    fcap_rev integer NOT NULL,
+    cap_rev integer NOT NULL,
+    capital integer NOT NULL,
+    bkvol integer NOT NULL,
+    ebook integer NOT NULL,
+    audio integer NOT NULL,
+    video integer NOT NULL,
+    databases integer NOT NULL,
+    subscrip integer NOT NULL,
+    hrs_open integer NOT NULL,
+    visits integer NOT NULL,
+    referenc integer NOT NULL,
+    regbor integer NOT NULL,
+    totcir integer NOT NULL,
+    kidcircl integer NOT NULL,
+    loanto integer NOT NULL,
+    loanfm integer NOT NULL,
+    totpro integer NOT NULL,
+    totatten integer NOT NULL,
+    gpterms integer NOT NULL,
+    pitusr integer NOT NULL,
+    yr_sub integer NOT NULL,
+    obereg varchar(2) NOT NULL,
+    rstatus integer NOT NULL,
+    statstru varchar(2) NOT NULL,
+    statname varchar(2) NOT NULL,
+    stataddr varchar(2) NOT NULL,
+    longitud numeric(10,7) NOT NULL,
+    latitude numeric(10,7) NOT NULL,
+    fipsst varchar(2) NOT NULL,
+    fipsco varchar(3) NOT NULL
+);
+
+CREATE INDEX libname2009_idx ON pls_fy2009_pupld09a (libname);
+CREATE INDEX stabr2009_idx ON pls_fy2009_pupld09a (stabr);
+CREATE INDEX city2009_idx ON pls_fy2009_pupld09a (city);
+CREATE INDEX visits2009_idx ON pls_fy2009_pupld09a (visits);
+
+COPY pls_fy2009_pupld09a
+FROM 'C:\Users\Y520\Documents\postgreSQL\pls_fy2009_pupld09a.csv'
+WITH (FORMAT CSV, HEADER);
+
+SELECT count(*)
+FROM pls_fy2014_pupld14a;
+
+SELECT count(*)
+FROM pls_fy2009_pupld09a;
+
+SELECT count(salaries)
+FROM pls_fy2014_pupld14a;
+
+SELECT count(libname)
+FROM pls_fy2014_pupld14a;
+
+SELECT count(DISTINCT libname)
+FROM pls_fy2014_pupld14a;
+
+SELECT max(visits), min(visits)
+FROM pls_fy2014_pupld14a;
+
+SELECT stabr
+FROM pls_fy2014_pupld14a
+GROUP BY stabr
+ORDER BY stabr;
+
+SELECT stabr
+FROM pls_fy2009_pupld09a
+GROUP BY stabr
+ORDER BY stabr;
+
+SELECT city, stabr, count(*)
+FROM pls_fy2014_pupld14a
+GROUP BY city, stabr
+ORDER BY count(*) DESC;
+
+SELECT stabr, count(*)
+FROM pls_fy2014_pupld14a
+GROUP BY stabr
+ORDER BY count(*) DESC;
+
+SELECT stabr, stataddr, count(*)
+FROM pls_fy2014_pupld14a
+GROUP BY stabr, stataddr
+ORDER BY stabr ASC, count(*) DESC;
+
+SELECT sum(visits) AS visits_2014
+FROM pls_fy2014_pupld14a
+WHERE visits >= 0;
+
+SELECT sum(visits) AS visits_2009
+FROM pls_fy2009_pupld09a
+WHERE visits >= 0;
+
+
+SELECT 
+(sum(pls09.visits)-sum(pls14.visits)) AS diff
+FROM pls_fy2014_pupld14a pls14 JOIN pls_fy2009_pupld09a pls09
+ON pls14.fscskey = pls09.fscskey
+WHERE pls14.visits >= 0 AND pls09.visits >= 0;
+
+SELECT pls14.stabr,
+sum(pls14.visits) AS visits_2014,
+sum(pls09.visits) AS visits_2009,
+round( (CAST(sum(pls14.visits) AS decimal(10,1)) - sum(pls09.visits)) /
+sum(pls09.visits) * 100, 2 ) AS pct_change
+FROM pls_fy2014_pupld14a pls14 JOIN pls_fy2009_pupld09a pls09
+ON pls14.fscskey = pls09.fscskey
+WHERE pls14.visits >= 0 AND pls09.visits >= 0
+GROUP BY pls14.stabr
+ORDER BY pct_change DESC;
+
+
+SELECT pls14.stabr,
+sum(pls14.gpterms) AS gpterms_2014,
+sum(pls09.gpterms) AS gpterms_2009,
+round( (CAST(sum(pls14.gpterms) AS decimal(10,1)) - sum(pls09.gpterms))
+	  /sum(pls09.gpterms) * 100, 2 ) AS pct_change
+FROM pls_fy2014_pupld14a pls14 JOIN pls_fy2009_pupld09a pls09
+ON pls14.fscskey = pls09.fscskey
+WHERE pls14.gpterms >= 0 AND pls09.gpterms >= 0
+GROUP BY pls14.stabr
+ORDER BY pct_change DESC;
+
+
+SELECT pls14.obereg,
+       sum(pls14.visits) AS visits_2014,
+       sum(pls09.visits) AS visits_2009,
+       round( (CAST(sum(pls14.visits) AS decimal(10,1)) - sum(pls09.visits)) /
+                    sum(pls09.visits) * 100, 2 ) AS pct_change
+FROM pls_fy2014_pupld14a pls14 JOIN pls_fy2009_pupld09a pls09
+ON pls14.fscskey = pls09.fscskey
+WHERE pls14.visits >= 0 AND pls09.visits >= 0
+GROUP BY pls14.obereg
+ORDER BY pct_change DESC;
+
+
+SELECT pls14.libname, pls14.city, pls14.stabr, pls14.statstru, pls14.c_admin, pls14.branlib,
+       pls09.libname, pls09.city, pls09.stabr, pls09.statstru, pls09.c_admin, pls09.branlib
+FROM pls_fy2014_pupld14a pls14 FULL OUTER JOIN pls_fy2009_pupld09a pls09
+ON pls14.fscskey = pls09.fscskey
+WHERE pls14.fscskey IS NULL OR pls09.fscskey IS NULL;
+
+
+CREATE TABLE meat_poultry_egg_inspect (
+    est_number varchar(50) CONSTRAINT est_number_key PRIMARY KEY,
+    company varchar(100),
+    street varchar(100),
+    city varchar(30),
+    st varchar(2),
+    zip varchar(5),
+    phone varchar(14),
+    grant_date date,
+    activities text,
+    dbas text
+);
+
+COPY meat_poultry_egg_inspect
+FROM 'C:\Users\Y520\Documents\postgreSQL\MPI_Directory_by_Establishment_Name.csv'
+WITH (FORMAT CSV, HEADER, DELIMITER ',');
+
+CREATE INDEX company_idx ON meat_poultry_egg_inspect (company);
+
+SELECT count(*) FROM meat_poultry_egg_inspect;
+
+SELECT company,
+       street,
+       city,
+       st,
+       count(*) AS address_count
+FROM meat_poultry_egg_inspect
+GROUP BY company, street, city, st
+HAVING count(*) > 1
+ORDER BY company, street, city, st;
+
+SELECT st, 
+       count(*) AS st_count
+FROM meat_poultry_egg_inspect
+GROUP BY st
+ORDER BY st;
+
+SELECT est_number,
+       company,
+       city,
+       st,
+       zip
+FROM meat_poultry_egg_inspect
+WHERE st IS NULL;
+
+SELECT est_number,
+       company,
+       city,
+       st,
+       zip
+FROM meat_poultry_egg_inspect
+WHERE zip = '55449' AND city = 'Blaine';
+
+UPDATE meat_poultry_egg_inspect
+set st = 'MN'
+WHERE city = 'Blaine' AND st is null;
+
+CREATE TABLE state_regions (
+    st varchar(2) CONSTRAINT st_key PRIMARY KEY,
+    region varchar(20) NOT NULL
+);
+
+COPY state_regions
+FROM 'C:\Users\Y520\Documents\postgreSQL\state_regions.csv'
+WITH (FORMAT CSV, HEADER, DELIMITER ',');
+
+
+CREATE TABLE acs_2011_2015_stats (
+    geoid varchar(14) CONSTRAINT geoid_key PRIMARY KEY,
+    county varchar(50) NOT NULL,
+    st varchar(20) NOT NULL,
+    pct_travel_60_min numeric(5,3) NOT NULL,
+    pct_bachelors_higher numeric(5,3) NOT NULL,
+    pct_masters_higher numeric(5,3) NOT NULL,
+    median_hh_income integer,
+    CHECK (pct_masters_higher <= pct_bachelors_higher)
+);
+
+COPY acs_2011_2015_stats
+FROM 'C:\Users\Y520\Documents\postgreSQL\acs_2011_2015_stats.csv'
+WITH (FORMAT CSV, HEADER, DELIMITER ',');
+
+SELECT * FROM acs_2011_2015_stats;
+
+
+ALTER TABLE meat_poultry_egg_inspect ADD COLUMN meat_processing boolean;
+ALTER TABLE meat_poultry_egg_inspect ADD COLUMN poultry_processing boolean;
+
+SELECT * FROM meat_poultry_egg_inspect; -- view table with new empty columns
+
+UPDATE meat_poultry_egg_inspect
+SET meat_processing = TRUE
+WHERE activities ILIKE '%meat processing%'; -- case-insensitive match with wildcards
+
+UPDATE meat_poultry_egg_inspect
+SET poultry_processing = TRUE
+WHERE activities ILIKE '%poultry processing%'; -- case-insensitive match with wildcards
+
+SELECT * FROM meat_poultry_egg_inspect;
+
+SELECT count(meat_processing), count(poultry_processing)
+FROM meat_poultry_egg_inspect;
+
+
+SELECT count(*)
+FROM meat_poultry_egg_inspect
+WHERE meat_processing = TRUE AND
+      poultry_processing = TRUE;
+	  
+	  
--- try_it_yourself_queries.sql
+++ try_it_yourself_queries.sql
@@ -114,6 +114,55 @@ COPY (SELECT geo_name, state_us_abbreviation, housing_unit_count_100_percent
 TO 'C:\Users\Y520\Documents\postgreSQL\TIY.txt'
 WITH (FORMAT CSV, HEADER)
 
+--8
 
+
+SELECT 
+(sum(pls09.visits)-sum(pls14.visits)) AS diff
+FROM pls_fy2014_pupld14a pls14 JOIN pls_fy2009_pupld09a pls09
+ON pls14.fscskey = pls09.fscskey
+WHERE pls14.visits >= 0 AND pls09.visits >= 0;
+
+SELECT pls14.stabr,
+sum(pls14.visits) AS visits_2014,
+sum(pls09.visits) AS visits_2009,
+round( (CAST(sum(pls14.visits) AS decimal(10,1)) - sum(pls09.visits)) /
+sum(pls09.visits) * 100, 2 ) AS pct_change
+FROM pls_fy2014_pupld14a pls14 JOIN pls_fy2009_pupld09a pls09
+ON pls14.fscskey = pls09.fscskey
+WHERE pls14.visits >= 0 AND pls09.visits >= 0
+GROUP BY pls14.stabr
+ORDER BY pct_change DESC;
+
+
+SELECT pls14.stabr,
+sum(pls14.gpterms) AS gpterms_2014,
+sum(pls09.gpterms) AS gpterms_2009,
+round( (CAST(sum(pls14.gpterms) AS decimal(10,1)) - sum(pls09.gpterms))
+	  /sum(pls09.gpterms) * 100, 2 ) AS pct_change
+FROM pls_fy2014_pupld14a pls14 JOIN pls_fy2009_pupld09a pls09
+ON pls14.fscskey = pls09.fscskey
+WHERE pls14.gpterms >= 0 AND pls09.gpterms >= 0
+GROUP BY pls14.stabr
+ORDER BY pct_change DESC;
+
+
+SELECT pls14.obereg,
+       sum(pls14.visits) AS visits_2014,
+       sum(pls09.visits) AS visits_2009,
+       round( (CAST(sum(pls14.visits) AS decimal(10,1)) - sum(pls09.visits)) /
+                    sum(pls09.visits) * 100, 2 ) AS pct_change
+FROM pls_fy2014_pupld14a pls14 JOIN pls_fy2009_pupld09a pls09
+ON pls14.fscskey = pls09.fscskey
+WHERE pls14.visits >= 0 AND pls09.visits >= 0
+GROUP BY pls14.obereg
+ORDER BY pct_change DESC;
+
+
+SELECT pls14.libname, pls14.city, pls14.stabr, pls14.statstru, pls14.c_admin, pls14.branlib,
+       pls09.libname, pls09.city, pls09.stabr, pls09.statstru, pls09.c_admin, pls09.branlib
+FROM pls_fy2014_pupld14a pls14 FULL OUTER JOIN pls_fy2009_pupld09a pls09
+ON pls14.fscskey = pls09.fscskey
+WHERE pls14.fscskey IS NULL OR pls09.fscskey IS NULL;
 
 
